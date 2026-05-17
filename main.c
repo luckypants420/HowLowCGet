8126d020d8b926387c34ea4215c5c146ebf65f95
Dhari <dhari2049@hotmail.com>
2026-05-17 20:05:44 +0100

    ints, floats

--- main.c
+++ main.c
@@ -4,6 +4,20 @@
 // expects an integer to return
 int main()
 {
-    printf("hey dhari, welcome to C language have fun mf \n get it in boiii");
+    /*
+    variable = reusable container for value
+    int = integer
+    float = decimal with floating point number with normal behavior of six digits shown on the right
+    */
+
+    int age = 27;
+    int year = 1998;
+    float gpa = 8.3;
+    float price = 9.99;
+    printf("hey dhari, welcome to C language have fun mf \n you are %d years old:", age);
+    printf("\nborn in year: %d", year);
+    printf("your gpa is %f:", gpa);
+    printf("the price of that toy is %.2f $:", price );
+    
     return 0;
 }
