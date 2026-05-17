1a344e4c77dcad566b59a3b420d66e3efd444145
Dhari <dhari2049@hotmail.com>
2026-05-17 19:53:30 +0100

    set up C language and gcc compiler

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // library for input and output
+
+// entry point for the program
+// expects an integer to return
+int main()
+{
+    printf("hey dhari, welcome to C language have fun mf \n get it in boiii");
+    return 0;
+}
