6fea3261e70dd96de53b8e5d3269caa2da58cffc
Dhari <dhari2049@hotmail.com>
2026-05-18 07:17:27 +0100

    swtich statemtn super easy u see

--- main.c
+++ main.c
@@ -3,41 +3,41 @@
 #include <string.h>  //library for strings
 #include <math.h>    //math library
 
-//weight converter 
+// weight converter
 
 int main()
 {
-    int choice = 0;
-    float pounds = 0.0f;
-    float kilograms = 0.0f;
+    // switch statemetns
 
-    printf("weight conversion calculator\n");
-    printf("1. kilograms in pounds\n ");
-    printf("2. pounds to kilograms\n ");
-    printf("enter your choice\n ");
+    int days = 0;
 
-    scanf("%d", &choice);
-    if (choice == 1)
+    printf("choose a day 1- 7");
+    scanf("%d", &days);
+    switch (days)
     {
-        // kilograms to pounds
-        printf("enter the weight in kilograms\n");
-
-        scanf("%f", &kilograms);
-        pounds = kilograms * 2.20462;
-        printf("%.2f kilograms is equal to %.2f pounds\n", kilograms, pounds);
+    case 1:
+        printf("its saturday");
+        break;
+    case 2:
+        printf("its sunday");
+        break;
+    case 3:
+        printf("its mon");
+        break;
+    case 4:
+        printf("its tue");
+        break;
+    case 5:
+        printf("its wed");
+        break;
+    case 6:
+        printf("its thur");
+        break;
+    case 7:
+        printf("its frid");
+        break;
+    default:
+        printf("you choose the worng thing mf, r u dumb, hchoose a numb");
     }
-    else if (choice == 2)
-    {
-        // pounds to kilograms
-        printf("enter the weight in pounds\n");
-        scanf("%f", &pounds);
-        kilograms = pounds / 2.20462;
-        printf("%.2f pounds is equal to %.2f kilograms\n", pounds, kilograms);
-    }
-    else
-    {
-        printf("invalid choice, please enter 1 or 2\n");
-    }
-
     return 0;
 }
