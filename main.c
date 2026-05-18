a0e48260ff1e595a2e253bf79d84ed25d631a8d4
Dhari <dhari2049@hotmail.com>
2026-05-18 21:20:58 +0100

    nested loop

--- main.c
+++ main.c
@@ -6,13 +6,19 @@
 int main()
 {
     // for loops = repeat code with a condition ( initilize; condition; update)
-//for loops are better used for a specific amount of time and while is for continuoius 
+    // for loops are better used for a specific amount of time and while is for continuoius
 
-    for (int i = 5; i >=0; i--)
+    for (int i = 5; i >= 0; i--)
     {
         Sleep(1000);
         printf("%d \n", i);
+        for (int j = 1; j < 10; j++)
+        {
+            Sleep(1000);
+            printf("%d", j);
+        }
+        printf("\n");
     }
-    printf("happy new year");
+
     return 0;
 }
