a2dc2299481afea3fef6df7b6fa90f89a74359ca
Dhari <dhari2049@hotmail.com>
2026-05-18 21:45:22 +0100

    random nums

--- main.c
+++ main.c
@@ -3,31 +3,17 @@
 #include <string.h>  //library for strings
 #include <math.h>    //math library
 #include <windows.h>
+#include <time.h>
+#include <stdlib.h>
 
-// break to stop a loopo
-//  continue to skip current cycle of loop
 int main()
 {
-    int rows = 0;
-    int columns = 0;
-    char symbol = '\0';
+    srand(time(NULL));
 
-    printf("enter the # of rows: ");
-    scanf("%d", &rows);
-
-    printf("enter the # of columns: ");
-    scanf("%d", &columns);
-
-    printf("enter the symbol you wnat to print: ");
-    scanf(" %c", &symbol);
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int i = 0; i < columns; i++)
-        {
-            printf("%c", symbol);
-        }
-        printf("\n");
-    }
+    int min = 1;
+    int max = 6;
+    // psudo random numbers
+    int randomNum = (rand() % (max - min + 1)) + min;
+    printf("%d", randomNum);
     return 0;
 }
