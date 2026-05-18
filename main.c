52e867d64423fa6ee429c3612b5ca8a60563251d
Dhari <dhari2049@hotmail.com>
2026-05-18 01:59:49 +0100

    simple if statments

--- main.c
+++ main.c
@@ -8,29 +8,21 @@
 int main()
 {
 
-    double principal = 0.0;
-    double rate = 0.0;
-    int years = 0;
-    int timesCompounded = 0;
-    double total = 0.0;
+    int age = 0;
 
-    printf("compund interests calculator\n ");
-
-    printf("enter the principal (p): ");
-    scanf("%lf", &principal);
-
-    printf("enter the interest rate % (r): ");
-    scanf("%lf", &rate);
-    rate = rate / 100;
-
-    printf("enter the # of years (t): ");
-    scanf("%d", &years);
-
-    printf("enter # of times compounded per year (n): ");
-    scanf("%d", &timesCompounded);
-
-    total = principal * pow(1 + rate/timesCompounded, timesCompounded * years);
-
-    printf("after %d years, the total will be $%.2lf", years, total);
+    printf("enter your age mf: ");
+    scanf("%d", &age);
+    if (age >= 18)
+    {
+        printf("you are an adult");
+    }
+    else if (age <= 0)
+    {
+        printf("buddy is not even born yet");
+    }
+    else
+    {
+        printf("you are a child buddy");
+    }
     return 0;
 }
