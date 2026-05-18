f5d7b803093df7d2ebbc4bf4f5f9c5ce45d50efa
Dhari <dhari2049@hotmail.com>
2026-05-18 23:28:56 +0100

    number guessing game

--- main.c
+++ main.c
@@ -16,7 +16,9 @@ int main()
     int min = 1;
     int max = 100;
     int answer = (rand() % (max - min + 1)) + min;
+
     printf("NUMBER GUESSING GAME\n ");
+    
     do
     {
         printf("guess a number between %d - %d", min, max);
